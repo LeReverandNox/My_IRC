ce2533c6c0e9c1caf2abdb89c1619baf75623993
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-04 20:25:06 +0100

    [] Setup the very base of the server,  few settings and start [#]

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+/*jslint node: true this:true es6:true */
+/*global this*/
+
+/// <reference path="../typings/index.d.ts" />
+
+"use strict";
+const Hapi = require("hapi");
+const config = require("./config");
+const plugins = require("./plugins");
+const routes = require("./routes");
+
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            cors: true
+        }
+    }
+});
+
+server.connection({
+    port: config.server.port
+});
+
+server.register(plugins, function (err) {
+    if (err) {
+        throw err;
+    }
+
+    server.route(routes);
+
+    server.start(function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log("The IRC app is now running on port " + server.info.port + " :)");
+    });
+});
