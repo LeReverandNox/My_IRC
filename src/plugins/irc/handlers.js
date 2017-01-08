6ece1922c52a4118a31bed3deb849110ae31c22a
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-08 23:24:04 +0100

    [] Add an handler module (server side) to handle socket events [#]

---
+++ src/plugins/irc/handlers.js
@@ -0,0 +1,15 @@
+/*jslint node: true this:true es6:true */
+/*global this*/
+
+"use strict";
+
+var handlers = function (server, ircService) {
+    return {
+        handleNewUser: function (socket) {
+            console.log("Here's a new connection");
+            return socket.emit("handshake", `Welcome to you sir, to My_IRC ! Feel free to join a channel (/help)`);
+        }
+    };
+};
+
+module.exports = handlers;
