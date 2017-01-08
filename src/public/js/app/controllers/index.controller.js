1d713086afbd332432d893baed89378bfec35e72
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-08 22:57:33 +0100

    [] Display a message on socket handshake in index view [#]

--- src/public/js/app/controllers/index.controller.js
+++ src/public/js/app/controllers/index.controller.js
@@ -8,9 +8,15 @@
         .module("my_irc")
         .controller("Index", IndexController);
 
-    function IndexController() {
-        var I = this;
+    IndexController.$inject = ["$rootScope", "indexIrcService"];
 
+    function IndexController($rootScope, indexIrcService) {
+        var I = this;
         I.title = "My_IRC";
+        I.message = "";
+
+        $rootScope.$on("handshake", function (e, data) {
+            I.message = data;
+        });
     }
 }());
