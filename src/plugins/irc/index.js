1364f869c8028cd15f5066a059cad6351b5bd10c
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-08 23:24:38 +0100

    [] Move the handshake into a handler, and instanciate handlers/service [#]

--- src/plugins/irc/index.js
+++ src/plugins/irc/index.js
@@ -5,13 +5,15 @@
 
 exports.register = function (server, options, next) {
     var io = require('socket.io')(server.listener);
+    var ircService = require("./services/ircService")(server);
+    var handlers = require("./handlers")(server, ircService);
+
+    ircService.init();
 
     io.on("connection", connectionHandler);
 
     function connectionHandler(socket) {
-        console.log("Here's a new connection");
-
-        socket.emit("handshake", `Welcome to you sir, to My_IRC ! Feel free to join a channel (/help)`);
+        handlers.handleNewUser(socket);
     }
 
     next();
