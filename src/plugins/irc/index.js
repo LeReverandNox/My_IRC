6a8c3758c3f6631aca07a56de07370164d7754e7
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-12 14:54:56 +0100

    [] Sors listener server-side, and add on for send private-messages [#]

--- src/plugins/irc/index.js
+++ src/plugins/irc/index.js
@@ -6,18 +6,30 @@
 exports.register = function (server, options, next) {
     var io = require('socket.io')(server.listener);
     var ircService = require("./services/ircService")(server, io).init();
-    var handlers = require("./handlers")(server, ircService);
+    var handlers = require("./handlers")(server, ircService, io);
 
     io.on("connection", connectionHandler);
 
     function connectionHandler(socket) {
         handlers.handleNewUser(socket);
 
+        /*
+         * Channels
+         */
         socket.on("joinChannel", handlers.joinChannel);
         socket.on("leaveChannel", handlers.leaveChannel);
         socket.on("listChannelUsers", handlers.listChannelUsers);
         socket.on("listChannels", handlers.listChannels);
+
+        /*
+         * Users
+         */
         socket.on("changeNickname", handlers.changeNickname);
+
+        /*
+         * Messages
+         */
+        socket.on("sendPrivateMessage", handlers.sendPrivateMessage);
 
         socket.on("disconnect", handlers.disconnect);
     }
