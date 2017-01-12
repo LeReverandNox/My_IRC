ab70091c18f754b01759335da7c5faf96e0cf6a8
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-12 17:05:22 +0100

    [] Add timestamp and horodatage to every log/message, easier to debug and log stuff :) [#]

--- src/plugins/irc/handlers.js
+++ src/plugins/irc/handlers.js
@@ -15,10 +15,11 @@ var handlers = function (server, ircService, io) {
             }
 
             var user = ircService.addUser(nickname, socket);
-            console.log(`${user.nickname} join the server !`);
+            console.log(`[${tools.datetime()}] - ${user.nickname} join the server !`);
             return socket.emit("handshake", {
                 message: `Welcome to you sir ${user.nickname}, to My_IRC ! Feel free to join a channel (/help)`,
-                nickname: user.nickname
+                nickname: user.nickname,
+                timestamp: tools.now()
             });
         },
         joinChannel: function (channel, cb) {
@@ -27,20 +28,21 @@ var handlers = function (server, ircService, io) {
 
             channel = channel.trim() || "";
             if (!channel) {
-                return cb("This channel name is too short !");
+                return cb({ message: "This channel name is too short !", timestamp: tools.now() });
             }
 
             ircService.joinChannel(user, channel, function (err, msg) {
                 if (err) {
-                    return cb(msg);
+                    return cb({ message: msg, timestamp: tools.now() });
                 }
-                console.log(`${user.nickname} join the channel ${channel} !`);
+                console.log(`[${tools.datetime()}] - ${user.nickname} join the channel ${channel} !`);
 
                 socket.broadcast.to(channel).emit("userJoinChannel", {
                     nickname: "SERVER",
-                    message: `${user.nickname} join the channel [${channel}]`
+                    message: `${user.nickname} join the channel [${channel}]`,
+                    timestamp: tools.now()
                 });
-                return cb(`You join the channel ${channel}`);
+                return cb({ message: `You join the channel ${channel}`, timestamp: tools.now() });
             });
         },
         leaveChannel: function (channel, cb) {
@@ -49,29 +51,32 @@ var handlers = function (server, ircService, io) {
 
             ircService.leaveChannel(user, channel, function (err, msg) {
                 if (err) {
-                    return cb(msg);
+                    return cb({ message: msg, timestamp: tools.now() });
                 }
-                console.log(`${user.nickname} left the channel ${channel} !`);
+                console.log(`[${tools.datetime()}] - ${user.nickname} left the channel ${channel} !`);
 
                 socket.broadcast.to(channel).emit("userLeftChannel", {
                     nickname: "SERVER",
-                    message: `${user.nickname} has left the channel [${channel}]`
+                    message: `${user.nickname} has left the channel [${channel}]`,
+                    timestamp: tools.now()
                 });
-                return cb(`You left the channel ${channel}`);
+                return cb({ message: `You left the channel ${channel}`, timestamp: tools.now() });
             });
         },
         listChannelUsers: function (channel, cb) {
             if (!ircService.channelExist(channel)) {
                 return cb({
                     message: "This channel doesn't exist !",
-                    data: []
+                    data: [],
+                    timestamp: tools.now()
                 });
             }
 
             var users = ircService.listChannelUsers(channel);
             return cb({
                 message: `Here the user list for channel [${channel}]`,
-                data: users
+                data: users,
+                timestamp: tools.now()
             });
         },
         listChannels: function (string, cb) {
@@ -88,33 +93,35 @@ var handlers = function (server, ircService, io) {
 
             return cb({
                 message: message,
-                data: channels
+                data: channels,
+                timestamp: tools.now()
             });
         },
         changeNickname: function (newNickname, cb) {
             var socket = this;
             var user = ircService.getUserBySocketId(socket.id);
             var oldNickname = user.nickname;
-            console.log(`L'user ${user.nickname} veut changer son nickname en ${newNickname}`);
+            console.log(`[${tools.datetime()}] - L'user ${user.nickname} veut changer son nickname en ${newNickname}`);
 
             newNickname = newNickname.trim() || "";
             if (!newNickname) {
-                return cb("This nickname is too short !");
+                return cb({ message: "This nickname is too short !", timestamp: tools.now() });
             }
 
             ircService.changeUserNickname(user, newNickname, function (err, msg) {
                 if (err) {
-                    return cb(msg);
+                    return cb({ message: msg, timestamp: tools.now() });
                 }
-                console.log(`${oldNickname} change is nickname to ${user.nickname} !`);
+                console.log(`[${tools.datetime()}] - ${oldNickname} change is nickname to ${user.nickname} !`);
 
                 user.channels.forEach(function (channel) {
                     socket.broadcast.to(channel).emit("hasChangeNickname", {
                         nickname: "SERVER",
-                        message: `${oldNickname} change is nickname to ${user.nickname} !`
+                        message: `${oldNickname} change is nickname to ${user.nickname} !`,
+                        timestamp: tools.now()
                     });
                 });
-                return cb(`You change your nickname from ${oldNickname} to ${user.nickname}`);
+                return cb({ message: `You change your nickname from ${oldNickname} to ${user.nickname}`, timestamp: tools.now() });
             });
         },
         sendPrivateMessage: function (to, content, cb) {
@@ -124,19 +131,20 @@ var handlers = function (server, ircService, io) {
             to = to.trim() || "";
             var toUser = ircService.getUserByNickname(to);
             if (!toUser) {
-                return cb(`The user ${to} doesn't exist.`);
+                return cb({ message: `The user ${to} doesn't exist.`, timestamp: tools.now() });
             }
 
             content = content.trim() || "";
             if (!content) {
-                return cb(`You can't send an empty private-message`);
+                return cb({ message: `You can't send an empty private-message`, timestamp: tools.now() });
             }
 
             io.to(toUser.socket.id).emit('receivePrivateMessage', {
                 nickname: fromUser.nickname,
-                message: content
+                message: content,
+                timestamp: tools.now()
             });
-            return cb(`Your message was delivered to ${to}`);
+            return cb({ message: `Your message was delivered to ${to}`, timestamp: tools.now() });
         },
         sendMessage: function (channel, content, cb) {
             var socket = this;
@@ -144,37 +152,39 @@ var handlers = function (server, ircService, io) {
 
             channel = channel.trim() || "";
             if (!ircService.channelExist(channel)) {
-                return cb(`The channel ${channel} doesn't exist.`);
+                return cb({ message: `The channel ${channel} doesn't exist.`, timestamp: tools.now() });
             }
 
             content = content.trim() || "";
             if (!content) {
-                return cb(`You can't send an empty message`);
+                return cb({ message: `You can't send an empty message`, timestamp: tools.now() });
             }
 
             io.to(channel).emit("receiveMessage", {
                 nickname: user.nickname,
                 message: content,
-                channel: channel
+                channel: channel,
+                timestamp: tools.now()
             });
-            return cb(`Your message was delivered`);
+            return cb({ message: `Your message was delivered`, timestamp: tools.now() });
         },
         disconnect: function () {
             var socket = this;
             var user = ircService.getUserBySocketId(socket.id);
-            console.log(`${user.nickname} left the server !`);
+            console.log(`[${tools.datetime()}] - ${user.nickname} left the server !`);
 
             user.channels.forEach(function (channel) {
                 ircService.leaveChannel(user, channel, function (err, msg) {
-                    console.log(`${user.nickname} left the channel ${channel} !`);
+                    console.log(`[${tools.datetime()}] - ${user.nickname} left the channel ${channel} !`);
                     socket.broadcast.to(channel).emit("userLeftChannel", {
                         nickname: "SERVER",
-                        message: `${user.nickname} has left the channel [${channel}]`
+                        message: `${user.nickname} has left the channel [${channel}]`,
+                        timestamp: tools.now()
                     });
                 });
             });
 
-            console.log(`${user.nickname} left the server !`);
+            console.log(`[${tools.datetime()}] - ${user.nickname} left the server !`);
             ircService.removeUser(user);
         }
     };
