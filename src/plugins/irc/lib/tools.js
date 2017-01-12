ecc818af2b5105dfbcb215acbaf0d1df006c4f5c
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-12 17:02:38 +0100

    [] Add 2 tools method to generate  datetime and timestamp for logging purposes [#]

--- src/plugins/irc/lib/tools.js
+++ src/plugins/irc/lib/tools.js
@@ -6,10 +6,42 @@
 var Chance = require("chance");
 var chance = new Chance();
 
+var leftPadDate = function (elem) {
+    if (elem < 10) {
+        elem = "0" + elem;
+    }
+    return elem;
+};
+
 var tools = {
     generateNickname: require("./nicknameGenerator"),
     generateHash: function () {
         return "#" + chance.zip();
+    },
+    now: function () {
+        return new Date().getTime();
+    },
+    datetime: function () {
+        var dateObj = new Date();
+
+        var month = (dateObj.getUTCMonth() + 1);
+        month = leftPadDate(month);
+
+        var day = dateObj.getUTCDate();
+        day = leftPadDate(day);
+
+        var year = dateObj.getUTCFullYear();
+
+        var hours = dateObj.getUTCHours();
+        hours = leftPadDate(hours);
+
+        var minutes = dateObj.getUTCMinutes();
+        minutes = leftPadDate(minutes);
+
+        var secondes = dateObj.getUTCSeconds();
+        secondes = leftPadDate(secondes);
+
+        return `${day}/${month}/${year} - ${hours}:${minutes}:${secondes}`;
     }
 };
 
