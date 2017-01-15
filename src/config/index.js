b7dc31cad4864c5c61daddbd4e638948e6b60cba
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-15 18:40:44 +0100

    [] Add 2 config variables for the server :giphy api_key and giphy url [#]

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 module.exports = {
     server: {
         port: 80
-    }
+    },
+    giphyAPIKey: process.env.GIPHY_API_KEY || "dc6zaTOxFJmzC",
+    giphyBaseURL: "http://api.giphy.com/v1/gifs/random"
 };
