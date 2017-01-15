4245121612662d6ef4eb555ac8842bb5c9243be4
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-15 16:14:28 +0100

    [] Adapt the socket routing to call .action on each handler [#]

--- src/plugins/irc/index.js
+++ src/plugins/irc/index.js
@@ -11,28 +11,30 @@ exports.register = function (server, options, next) {
     io.on("connection", connectionHandler);
 
     function connectionHandler(socket) {
-        handlers.handleNewUser(socket);
+        handlers.handleNewUser.action(socket);
 
         /*
          * Channels
          */
-        socket.on("joinChannel", handlers.joinChannel);
-        socket.on("leaveChannel", handlers.leaveChannel);
-        socket.on("listChannelUsers", handlers.listChannelUsers);
-        socket.on("listChannels", handlers.listChannels);
+        socket.on("joinChannel", handlers.joinChannel.action);
+        socket.on("leaveChannel", handlers.leaveChannel.action);
+        socket.on("listChannelUsers", handlers.listChannelUsers.action);
+        socket.on("listChannels", handlers.listChannels.action);
 
         /*
          * Users
          */
-        socket.on("changeNickname", handlers.changeNickname);
+        socket.on("changeNickname", handlers.changeNickname.action);
 
         /*
          * Messages
          */
-        socket.on("sendPrivateMessage", handlers.sendPrivateMessage);
-        socket.on("sendMessage", handlers.sendMessage);
+        socket.on("sendPrivateMessage", handlers.sendPrivateMessage.action);
+        socket.on("sendMessage", handlers.sendMessage.action);
 
-        socket.on("disconnect", handlers.disconnect);
+        socket.on("listCommands", handlers.listCommands.action);
+
+        socket.on("disconnect", handlers.disconnect.action);
     }
 
     next();
