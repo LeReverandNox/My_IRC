6f08ac9efe1a9e7efb0a012ae2b26737bfde7c05
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-16 20:47:05 +0100

    [] add an APPLICATION_ENV parameter in the config, to determine wich routes to load, to implemement [#]

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,6 @@ module.exports = {
         port: process.env.SERVER_PORT || 80
     },
     giphyAPIKey: process.env.GIPHY_API_KEY || "dc6zaTOxFJmzC",
-    giphyBaseURL: process.env.GIPHY_BASE_URL || "http://api.giphy.com/v1/gifs/random"
+    giphyBaseURL: process.env.GIPHY_BASE_URL || "http://api.giphy.com/v1/gifs/random",
+    applicationEnv: process.env.APPLICATION_ENV || "production"
 };
