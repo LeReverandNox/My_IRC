d64be020931ce61542a1b8162dfb0d325216dcae
laidet_r <rodolphe.laidet@epitech.eu>
2017-01-22 22:34:33 +0100

    [] Update the content formatter to only add \n between each line of the same content [#]

--- src/public/js/app/services/formatter.service.js
+++ src/public/js/app/services/formatter.service.js
@@ -83,7 +83,7 @@
 
             if (data) {
                 data.forEach(function (el) {
-                    content = content + "\n\r - " + el;
+                    content = content + "\n - " + el;
                 });
             }
 
